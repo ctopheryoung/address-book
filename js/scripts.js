412b450dd65681d0dcddaed80f4dd09283dbe3a6
Chris Young and Gracie Lundell <ctopheryoung@gmail.com>
2016-01-25 08:46:35 -0800

    Add UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,18 @@ function Contact(firstName, lastName) {
   this.lastName = lastName;
   this.addresses = [];
 }
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var addedFirstName = $("input#new-first-name").val();
+    var addedLastName = $("input#new-last-name").val();
+    var newContacts = new Contact(addedFirstName, addedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>") + newContacts.fullName() + ("</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  })
+})
